d01f07d04c3acb4648ae09806ecfca60ec9223eb
ice0226 <aya.ogasawara.0226@gmail.com>
2023-08-02 19:53:03 +0900

    save

---
+++ resources/views/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        　index
+    </x-slot>
+        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
+        <h1>Live Name</h1>
+        <div class='posts'>
+            @foreach ($posts as $post)
+                <div class='post'>
+                    <h2 class='title'><a href="/posts/{{ $post->id }}">{{ $post->title }}</a></h2>
+                    <p class='body'>{{ $post->body }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
+        </div>
+        <a href='/posts/create'>create</a>
+</x-app-layout>
